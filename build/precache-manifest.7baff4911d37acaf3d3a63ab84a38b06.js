37b885ccb5bf1f2731987c976ad0fbfc1483fc8f
yu kuai <yukuai@yudeMacBook-Pro.local>
2019-10-08 21:25:00 +0800

    build

---
+++ build/precache-manifest.7baff4911d37acaf3d3a63ab84a38b06.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913dd1c92dc053ec9eb5ca21be8228f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40cab92c9b834afdd8a8",
+    "url": "/static/css/2.6b9f4f4d.chunk.css"
+  },
+  {
+    "revision": "17fcab82b09f354591bf",
+    "url": "/static/css/main.5b80254d.chunk.css"
+  },
+  {
+    "revision": "40cab92c9b834afdd8a8",
+    "url": "/static/js/2.84026632.chunk.js"
+  },
+  {
+    "revision": "17fcab82b09f354591bf",
+    "url": "/static/js/main.334063b4.chunk.js"
+  },
+  {
+    "revision": "eb98a4f675d2e4824cff",
+    "url": "/static/js/runtime-main.a6aef668.js"
+  },
+  {
+    "revision": "294421579e03d49e799b24ec5a28a8d7",
+    "url": "/static/media/1.29442157.jpeg"
+  },
+  {
+    "revision": "475f1ac7752c83ac82f0f7b2927ff1ef",
+    "url": "/static/media/5.475f1ac7.jpeg"
+  },
+  {
+    "revision": "9dcf789ea39e44a3ba22e86878ceb6b1",
+    "url": "/static/media/6.9dcf789e.jpeg"
+  },
+  {
+    "revision": "3e187a3fac58515c59fe0d3e9b1b2bc2",
+    "url": "/static/media/7.3e187a3f.jpeg"
+  },
+  {
+    "revision": "9a1d3b2c8652c71928914fe8bbe7a491",
+    "url": "/static/media/bgi.9a1d3b2c.jpg"
+  },
+  {
+    "revision": "98d4959f01769216ee1eb1d47eb25d9d",
+    "url": "/static/media/we.98d4959f.jpeg"
+  },
+  {
+    "revision": "75359ddb7e18bb2290e91ea7ff73e1f8",
+    "url": "/static/media/wx.75359ddb.jpg"
+  },
+  {
+    "revision": "75df85d13dfb8d46a8574ea6cda8c51a",
+    "url": "/static/media/zfb.75df85d1.jpg"
+  }
+]);
